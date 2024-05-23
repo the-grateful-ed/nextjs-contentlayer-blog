abfdd82fd29af8339a80da1a216db4055a1bfaad
edndacomputer <82444464+the-grateful-ed@users.noreply.github.com>
2024-05-22 23:47:02 -0700

    Update mdx-components.tsx
    Added Twitter and YouTube components

--- components/mdx-components.tsx
+++ components/mdx-components.tsx
@@ -1,16 +1,20 @@
-import Image from "next/image"
-import { useMDXComponent } from "next-contentlayer/hooks"
+import Image from "next/image";
+import { useMDXComponent } from "next-contentlayer/hooks";
+import YouTube from "../components/YouTube";
+import Twitter from "../components/Twitter";
 
 const components = {
   Image,
-}
+  YouTube,
+  Twitter,
+};
 
 interface MdxProps {
-  code: string
+  code: string;
 }
 
 export function Mdx({ code }: MdxProps) {
-  const Component = useMDXComponent(code)
+  const Component = useMDXComponent(code);
 
-  return <Component components={components} />
+  return <Component components={components} />;
 }
